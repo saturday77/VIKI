1bba39bcd5a1fd3159010f07b83e0089c48b3ed1
saturday77 <drlee@live.ru>
2023-04-17 15:34:41 +0300

    -

--- VIKI.py
+++ VIKI.py
@@ -1,1 +1,12 @@
-print("hi")
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.Sequential([
+    keras.layers.Embedding(input_dim=10000, output_dim=16),
+    keras.layers.GlobalAveragePooling1D(),
+    keras.layers.Dense(units=16, activation='relu'),
+    keras.layers.Dense(units=1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=16, validation_data=(x_test, y_test))
